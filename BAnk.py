6bbebd112fb417fc5dbb64029c7459e73db47abc
hridoy <ariyanhridoy921@gmail.com>
2026-02-24 02:06:39 +0600

    bank class created

---
+++ BAnk.py
@@ -0,0 +1,10 @@
+class bankaccount:
+    
+    def __int__(self , accountNumber , name , balance):
+        self.accountNumber = accountNumber
+        self.name = name 
+        self.balance = balance
+        
+    def display(self):
+            
+        
