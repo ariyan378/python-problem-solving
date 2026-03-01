ba9e178428961cb7e7145fe29c9fb4ead3c4ad25
hridoy <ariyanhridoy921@gmail.com>
2026-03-01 22:00:50 +0600

    json oop

---
+++ class_json.py
@@ -0,0 +1,21 @@
+import json
+
+class Person:
+    def __init__(self, name, id):
+        self.name = name 
+        self.id = id
+
+person = Person('Hridoy', 378)
+
+def display_object(obj): 
+    if isinstance(obj, Person):
+  
+        return "My name is {} . My Id is {}".format(obj.name, obj.id)
+
+with open('Object.json', 'w') as f:
+
+    json.dump(person, f, default=display_object) 
+         
+with open('Object.json', 'r') as f:
+    text = json.load(f)
+    print(text)
