2a180d88acde3707175a10c9801b3bcc5e20981d
hridoy <ariyanhridoy921@gmail.com>
2026-03-02 08:38:39 +0600

    File Handling

---
+++ student_attendance_filehandling.py
@@ -0,0 +1,29 @@
+import os
+
+file_name = 'Student.txt'
+
+print('How Many names do you want to add:')
+choice = int(input())
+names = set()
+
+for i in range(choice):
+    entry = input(f'Enter Name {i+1}: ').strip().capitalize()
+    names.add(entry)
+
+if os.path.exists(file_name):
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+        entry_count = len(lines) + 1
+else:
+    entry_count = 1
+
+names_string = ", ".join(names)
+
+with open(file_name, 'a') as f:
+    f.write(f"Entry No {entry_count} -> {names_string}\n")
+
+print(f"\n--- Success! Entry {entry_count} saved ---")
+
+print("\n--- Current Student.txt Content ---")
+with open(file_name, 'r') as f:
+    print(f.read())
