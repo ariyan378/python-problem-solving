0e8a7f6c13c5bd29c0c4ab14f927e70987017f56
hridoy <ariyanhridoy921@gmail.com>
2026-03-08 04:27:06 +0600

    Enjoyed that

---
+++ Decorator_demo.py
@@ -0,0 +1,35 @@
+import time
+
+def timer(func):
+    
+    def wrapper(*args):
+        start = time.time()
+
+        func(*args)
+
+        print('Time Taken By', func.__name__ , time.time()-start , 'secs')
+        print("___________________")
+    
+    return wrapper
+
+@timer
+def hello():
+    
+    print('Hello Himuuuu') 
+    time.sleep(1)
+    
+@timer
+def Barcelona():
+    
+    print("Lionel Messi The Goat") 
+    time.sleep(1)   
+
+@timer
+def square (num):
+    
+    time.sleep(1)
+    print(num**2)    
+
+hello()    
+Barcelona()
+square(111)   
